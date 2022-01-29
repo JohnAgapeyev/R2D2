2dda9d80f294bb908b7015ea429bf3bd75dc37bf
John Agapeyev <jagapeyev@gmail.com>
2022-01-28 19:32:11 -0800

    Cleaned up the dir copy api

--- src/main.rs
+++ src/main.rs
@@ -47,32 +47,8 @@ fn generate_temp_folder_name() -> PathBuf {
 //    files.append(get_dir_tree);
 //}
 
-fn copy_dir() -> io::Result<()> {
-    let dest = generate_temp_folder_name();
-
-    //let (dirs, files): (Vec<DirEntry>, Vec<DirEntry>) = fs::read_dir(env::current_dir()?)?
-    //    .filter_map(|entry| entry.ok())
-    //    .filter(|entry| entry.file_type().is_ok())
-    //    .filter(|entry| {
-    //        entry
-    //            .file_name()
-    //            .into_string()
-    //            .unwrap_or(String::new())
-    //            .chars()
-    //            .nth(0)
-    //            .unwrap_or('a')
-    //            != '.'
-    //    })
-    //    .partition(|entry| entry.file_type().unwrap().is_dir());
-
-    //let df: Vec<DirEntry> = dirs
-    //    .iter()
-    //    .flat_map(|dir| fs::read_dir(dir.path()))
-    //    .filter_map(|entry| entry.ok())
-    //    //.map(|entry| entry.ok())
-    //    //.collect::<Vec<_>>();
-    //    .collect();
-    let files: Vec<_> = WalkDir::new(env::current_dir()?)
+fn copy_dir(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
+    let files: Vec<_> = WalkDir::new(from)
         .into_iter()
         .filter_entry(|e| {
             !e.file_name()
@@ -89,20 +65,18 @@ fn copy_dir() -> io::Result<()> {
         ));
     }
 
-    DirBuilder::new().recursive(true).create(&dest)?;
-
     let (dirs, files): (Vec<DirEntry>, Vec<DirEntry>) = files
         .into_iter()
         .map(|f| f.unwrap())
         .partition(|e| e.file_type().is_dir());
 
     dirs.into_iter().for_each(|d| {
-        let mut dest_dir = String::from(dest.to_str().unwrap());
+        let mut dest_dir = String::from(to.to_str().unwrap());
         dest_dir.push_str(
             d.path()
                 .to_str()
                 .unwrap()
-                .strip_prefix(env::current_dir().unwrap().to_str().unwrap())
+                .strip_prefix(from.to_str().unwrap())
                 .unwrap(),
         );
 
@@ -115,12 +89,12 @@ fn copy_dir() -> io::Result<()> {
     files
         .into_iter()
         .for_each(|f| {
-            let mut dest_file = String::from(dest.to_str().unwrap());
+            let mut dest_file = String::from(to.to_str().unwrap());
             dest_file.push_str(
                 f.path()
                     .to_str()
                     .unwrap()
-                    .strip_prefix(env::current_dir().unwrap().to_str().unwrap())
+                    .strip_prefix(from.to_str().unwrap())
                     .unwrap(),
             );
 
@@ -128,11 +102,9 @@ fn copy_dir() -> io::Result<()> {
             OpenOptions::new()
                 .write(true)
                 .create_new(true)
-                .open(&dest_file);
+                .open(&dest_file).unwrap();
             fs::copy(f.path(), dest_file);
         });
-
-    fs::remove_dir_all(dest)?;
 
     Ok(())
 }
@@ -140,7 +112,14 @@ fn copy_dir() -> io::Result<()> {
 fn main() -> io::Result<()> {
     println!("Hello world");
 
-    copy_dir()?;
+    let src = env::current_dir()?;
+    let dest = generate_temp_folder_name();
+
+    DirBuilder::new().recursive(true).create(&dest)?;
+
+    copy_dir(&src, &dest)?;
+
+    fs::remove_dir_all(dest)?;
 
     Ok(())
 }
