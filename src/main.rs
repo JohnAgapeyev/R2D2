8f50ee058410e6194edccd3e0656389c959b6118
John Agapeyev <jagapeyev@gmail.com>
2022-01-29 18:05:07 -0800

    Leverage cargo metadata for finding the root of the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use camino::Utf8PathBuf;
+use cargo_metadata::MetadataCommand;
 use r2d2::obfuscate;
 use std::env;
 use std::fs;
@@ -7,13 +9,12 @@ use std::io;
 use std::io::ErrorKind;
 use std::io::Write;
 use std::path::Path;
-use std::path::PathBuf;
 use std::process::Command;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
 
-fn generate_temp_folder_name() -> PathBuf {
-    let mut output = env::temp_dir();
+fn generate_temp_folder_name() -> Utf8PathBuf {
+    let mut output = Utf8PathBuf::from_path_buf(env::temp_dir()).unwrap();
     output.push(".r2d2_build_dir");
     output
 }
@@ -21,7 +22,7 @@ fn generate_temp_folder_name() -> PathBuf {
 //TODO: Only copy differences with hashes/mtime checks
 //TODO: This needs to be optimized and cleaned up
 //TODO: Fix the error checking
-fn copy_dir(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
+fn copy_dir(from: &Utf8PathBuf, to: &Utf8PathBuf) -> io::Result<()> {
     let files: Vec<_> = WalkDir::new(from)
         .into_iter()
         .filter_entry(|e| {
@@ -50,13 +51,13 @@ fn copy_dir(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
                 d.path()
                     .to_str()
                     .unwrap()
-                    .strip_prefix(from.to_str().unwrap())
+                    .strip_prefix(&from.to_string())
                     .unwrap(),
             )
         })
         .filter(|filename| !filename.contains("/target"))
         .for_each(|filename| {
-            let mut dest_dir = String::from(to.to_str().unwrap());
+            let mut dest_dir = to.to_string();
 
             dest_dir.push_str(&filename);
 
@@ -71,16 +72,16 @@ fn copy_dir(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
                 f.path()
                     .to_str()
                     .unwrap()
-                    .strip_prefix(from.to_str().unwrap())
+                    .strip_prefix(&from.to_string())
                     .unwrap(),
             )
         })
         .filter(|filename| !filename.contains("/target"))
         .for_each(|filename| {
-            let mut dest_file = String::from(to.to_str().unwrap());
+            let mut dest_file = to.to_string();
             dest_file.push_str(&filename);
 
-            let mut src_file = String::from(from.to_str().unwrap());
+            let mut src_file = from.to_string();
             src_file.push_str(&filename);
 
             OpenOptions::new()
@@ -101,8 +102,14 @@ fn copy_dir(from: &PathBuf, to: &PathBuf) -> io::Result<()> {
     Ok(())
 }
 
+fn get_src_dir() -> Utf8PathBuf {
+    let metadata = MetadataCommand::new().exec().unwrap();
+    println!("Root is at {}", metadata.workspace_root);
+    metadata.workspace_root
+}
+
 fn main() -> io::Result<()> {
-    let src = env::current_dir()?;
+    let src = get_src_dir();
     let dest = generate_temp_folder_name();
 
     if let Ok(_) = std::fs::metadata(&dest) {
@@ -117,7 +124,7 @@ fn main() -> io::Result<()> {
     let output = Command::new("cargo")
         .arg("build")
         .arg("--target-dir")
-        .arg(format!("{}/target", src.to_str().unwrap()))
+        .arg(format!("{}/target", src.to_string()))
         .current_dir(&dest)
         .output()
         .expect("failed to execute process");
