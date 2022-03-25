8f94f6336c851e09bad456937658154d536253f1
John Agapeyev <jagapeyev@gmail.com>
2022-03-25 08:14:18 -0700

    Got a venue for functional tests, and reworked test driver to be a bit cleaner

--- tests/test_driver.rs
+++ tests/test_driver.rs
@@ -1,13 +1,20 @@
 use camino::Utf8PathBuf;
+use lazy_static::lazy_static;
 use r2d2::*;
 use std::fs;
 use std::fs::DirBuilder;
 use std::io;
 use std::process::Command;
 use std::process::Stdio;
+use std::sync::Mutex;
 
-#[test]
-fn main() -> io::Result<()> {
+const TEST_DIRECTORY: &str = ".r2d2_test_dir";
+
+lazy_static! {
+    static ref FILESYSTEM_MUTEX: Mutex<()> = Mutex::new(());
+}
+
+fn setup_test_crate(path: &str) -> io::Result<(Utf8PathBuf, Utf8PathBuf)> {
     /*
      * Don't need to override src dir due to dependency resolution
      * We rely on r2d2 crate for all the test crates
@@ -15,7 +22,7 @@ fn main() -> io::Result<()> {
      * Setting the cargo current_dir for building is adequate to avoid out-of-scope builds/tests
      */
     let src = get_src_dir();
-    let dest = generate_temp_folder_name(Some(".r2d2_test_dir"));
+    let dest = generate_temp_folder_name(Some(TEST_DIRECTORY));
 
     if let Ok(_) = std::fs::metadata(&dest) {
         //Clean up the folder if it already exists
@@ -24,26 +31,82 @@ fn main() -> io::Result<()> {
 
     DirBuilder::new().recursive(true).create(&dest)?;
 
-    //TODO: Make this nice with concat! instead of full path prints
-    //let true_dest = Utf8PathBuf::from("/tmp/.r2d2_test_dir/tests/single/01-hello_world");
-    let true_dest = Utf8PathBuf::from("/tmp/.r2d2_test_dir/tests/single/02-prints");
+    let mut true_dest_str = String::from(dest.as_str());
+    true_dest_str.push('/');
+    true_dest_str.push_str(path);
+
+    let true_dest = Utf8PathBuf::from(true_dest_str);
 
     copy_dir(&src.workspace_root, &dest)?;
     obfuscate_dir(&true_dest)?;
 
-    println!("Calling cargo");
+    Ok((src.target_dir, true_dest))
+}
 
-    Command::new("cargo")
-        .arg("build")
-        .arg("--target-dir")
-        .arg(&src.target_dir)
-        .current_dir(&true_dest)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("failed to execute process");
+mod simple {
+    use crate::*;
 
-    println!("Process is done");
+    #[test]
+    fn hello_world() {
+        let _lock = FILESYSTEM_MUTEX.lock().unwrap();
+        let (src, dest) = setup_test_crate("tests/single/01-hello_world").unwrap();
 
-    Ok(())
+        println!("Calling cargo");
+
+        Command::new("cargo")
+            .arg("build")
+            .arg("--target-dir")
+            .arg(&src)
+            .current_dir(&dest)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to execute process");
+
+        println!("Process is done");
+
+        Command::new("cargo")
+            .arg("run")
+            .arg("--target-dir")
+            .arg(&src)
+            .current_dir(&dest)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to execute process");
+
+        println!("Execution is done");
+    }
+
+    #[test]
+    fn prints() {
+        let _lock = FILESYSTEM_MUTEX.lock().unwrap();
+        let (src, dest) = setup_test_crate("tests/single/02-prints").unwrap();
+
+        println!("Calling cargo");
+
+        Command::new("cargo")
+            .arg("build")
+            .arg("--target-dir")
+            .arg(&src)
+            .current_dir(&dest)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to execute process");
+
+        println!("Process is done");
+
+        Command::new("cargo")
+            .arg("run")
+            .arg("--target-dir")
+            .arg(&src)
+            .current_dir(&dest)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to execute process");
+
+        println!("Execution is done");
+    }
 }
